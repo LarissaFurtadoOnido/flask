903a2a4706c7bf4fcc5906dc57a8bb452542f820
LarissaFurtadoOnido <olarissafurtado@gmail.com>
2024-09-26 16:25:16 -0300

    up

--- server.py
+++ server.py
@@ -2,10 +2,18 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET', 'POST'])
-
+@app.route("/", methods = ['GET'])
 def homepage():
     if request.method == 'GET':
-        return render_template('index.html')
+        return "render_template('index.html')"
+
+
+@app.route("/convidados", methods = ['POST'])
+def convidados():
     
-    
+
+
+
+
+ if __name__ == "__main__":
+    app.run(debug=True)
